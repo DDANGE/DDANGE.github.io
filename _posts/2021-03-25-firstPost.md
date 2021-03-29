a7268abdcc22fe417b66a6a4a46238678be80714
Kim deokyong <royalkdy@gmail.com>
2021-03-30 03:00:49 +0900

    category & tag archive설정

---
+++ _posts/2021-03-25-firstPost.md
@@ -0,0 +1,17 @@
+---
+title: First Post
+date: 2021-03-25 07:00:00 +0900
+categories: [Unity]
+tags: [Test]
+#class: post-template
+#subclass: 'post tag-Unity'
+author: ddange
+---
+First Post
+=========
+```c#
+Console.Writeline("C# Test");
+public class Test{
+    private string name;
+}
+```
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,13 @@
-var store = []
+var store = [{
+        "title": "GitHubPage 첫번째 포스트",
+        "excerpt":"깃허브 페이지 시작하기 이전 회사에서 협업툴로 SVN을 사용해 보았지만, 깃허브를 많이 사용했다. 포트폴리오 관리나 주기적으로 공부하기 위해서 필수였다. 깃허브를 공부하고 잘 사용하지 않아 공부했던 내용이 잊혀지기 일수였다. 그래서 블로그를 깃허브 페이지로 옮기고 깃에 업데이트 하기로 했다 오늘은 첫번째 포스트일텐데 여러가지 마크다운 문법을 적용해보고 소스트리를 통해 업로드 할 생각이다. c#언어 코드...","categories": ["Unity"],
+        "tags": ["Unity"],
+        "url": "/unity/testPost/",
+        "teaser": null
+      },{
+        "title": "First Post",
+        "excerpt":"First Post  Console.Writeline(\"C# Test\"); public class Test{     private string name; }  ","categories": ["Unity"],
+        "tags": ["Test"],
+        "url": "/unity/firstPost/",
+        "teaser": null
+      }]
