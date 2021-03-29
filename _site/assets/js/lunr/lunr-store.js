34fd5108abc435e83bf40b4181829fe87cac1762
Kim deokyong <royalkdy@gmail.com>
2021-03-30 05:19:45 +0900

    수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "GitHubPage로 이사",
-        "excerpt":"-깃허브 페이지 시작 깃허브에 익숙해지기 위해 블로그도 이사를 하게 됐다. -깃허브를 사용하면서… 네이버블로그를 쓰던 나는 소스코드를 남기는게 많이 불편했다. 깃페이지는 아래처럼 마크다운 문법에 따라 키워드만 입력해주면 코드를 보기쉽게 포스팅 할 수 있었다. c#언어 코드 테스트 Console.WriteLine(\"C# 코드 테스트\"); public class Test { } c언어 코드 테스트 printf(\"C언어 코드 테스트\"); 이직을...","categories": ["Unity"],
+        "excerpt":"깃허브 페이지 시작 깃허브에 익숙해지기 위해 블로그도 이사를 하게 됐다. 깃허브를 사용하면서… 네이버블로그를 쓰던 나는 소스코드를 남기는게 많이 불편했다. 깃페이지는 아래처럼 마크다운 문법에 따라 키워드만 입력해주면 코드를 보기쉽게 포스팅 할 수 있었다. c#언어 코드 테스트 Console.WriteLine(\"C# 코드 테스트\"); public class Test { } c언어 코드 테스트 printf(\"C언어 코드 테스트\"); 이직을...","categories": ["Unity"],
         "tags": ["Unity"],
         "url": "/unity/testPost/",
         "teaser": null
